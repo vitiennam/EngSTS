4377af3eff681fdb532b19937ace9195c22c8adb
Nam Vi <vinam@MacBook-Air-cua-vi.local>
2023-01-11 14:44:26 +0700

    change html index

--- startup-script.sh
+++ startup-script.sh
@@ -1,6 +1,9 @@
+
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/app
 sudo cat >/etc/supervisor/conf.d/node-app.conf << EOF
 [program:nodeapp]
-directory=/home/vitiennam/EngSTS
+directory=/opt/app/EngSTS
 command=npm run server
 autostart=true
 autorestart=true
@@ -12,3 +15,22 @@ EOF
 
 supervisorctl reread
 supervisorctl update
+
+# ------------------------- Systemctl
+sudo nano /lib/systemd/system/engs_env.service
+
+[Unit]
+Description=engs_env. - making your environment variables rad
+Documentation=https://example.com
+After=network.target
+
+[Service]
+Environment=NODE_PORT=80
+Type=simple
+User=vitiennam
+WorkingDirectory=/opt/app/EngSTS/
+ExecStart=sudo node server.js
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
