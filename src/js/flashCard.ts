a137ccbea139943631211961ff2c783ae2738994
Nam Vi <vinam@MacBook-Air-cua-vi.local>
2023-02-05 15:16:52 +0700

    end flip card, change to click card

--- src/js/flashCard.ts
+++ src/js/flashCard.ts
@@ -69,7 +69,33 @@ function flashCardToggle(this: GlobalEventHandlers,evt: MouseEvent){
     }
   }
 }
-
+function changeWord(this: GlobalEventHandlers, evt: MouseEvent){
+  // let targetTag = evt.target as HTMLButtonElement | null
+  index += 1
+  document.getElementById("nextword").innerHTML ='Click '+ randomArray[index]
+  userHistory.push(randomArray[index])
+  document.getElementById('iframeContent').setAttribute('src', "https://dictionary.cambridge.org/dictionary/english-vietnamese/" + randomArray[index])
+  if(index == randomArray.length - 1)
+  {
+    index = -1
+    
+    fetch('/randomWord',{
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    
+    }).then((response)=> response.json()).then((data)=> {
+      randomArray = data.map((word: { word: string }) => word.word)
+    })
+  }
+}
 // after document loaded
 
 document.onreadystatechange = () => {
@@ -105,20 +131,19 @@ document.onreadystatechange = () => {
 
     console.log(randomArray)
     index += 1
-    document.getElementById("randomText").innerHTML = randomArray[index]
+    document.getElementById("nextword").innerHTML ='Click '+ randomArray[index]
     userHistory.push(randomArray[index])
     document.getElementById('iframeContent').setAttribute('src', "https://dictionary.cambridge.org/dictionary/english-vietnamese/" + randomArray[index])
 
-    setTimeout(clickAcceptCookie, 2000)
-    // document.getElementById('onetrust-accept-btn-handler').click()
 
   })
   document.getElementById("hamburger").onclick = navSideBtn
   document.getElementById("logOut").onclick = logOut
-  let btnList = document.getElementById("containerFlashCard").getElementsByTagName("button")
-  for(let i = 0; i < btnList.length; i++){
-    btnList[i].onclick = flashCardToggle
-  }
+  document.getElementById("nextword").onclick = changeWord
+  // let btnList = document.getElementById("containerFlashCard").getElementsByTagName("button")
+  // for(let i = 0; i < btnList.length; i++){
+  //   btnList[i].onclick = flashCardToggle
+  // }
   let isOnIOS = navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPhone/i)
   let eventName = isOnIOS ? "pagehide" : "beforeunload"
   window.addEventListener(eventName, function (event) { 
